9f5c39d1d948febf40e6dd4879f51cda98a8dce1
ridhampansuriya2000 <ridhampansuriya2000@gmail.com>
2023-12-08 18:36:21 +0530

    change card color

--- src/components/CardOne.tsx
+++ src/components/CardOne.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 const CardOne = ({data} : any) => {
   return (
-    <div className="rounded-md border  border-white bg-boxdark py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark ">
-      <div className=" flex justify-center">
-        <h6 className="text-center text-white dark:text-black ">Start Date</h6>
-      </div>
-
-      <div className="rounded-lg bg-[black] shadow-default mt-4">
-        <div className="flex p-10 justify-center bg-black rounded-lg">
-          <p className="text-center text-white dark:text-black font-medium text-title-sm">
-            1 Feb, 2022
-          </p>
+    <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
+      <div className="mt-4 flex items-end justify-between">
+        <div>
+          <h4 className="text-title-md font-bold text-black dark:text-white">
+            {data.data}
+          </h4>
+          <span className="text-sm font-medium">{data.label}</span>
         </div>
       </div>
     </div>
