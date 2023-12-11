0e7d53d5e957502c45d0451a4b4f70f8a617eb1f
ridhampansuriya2000 <ridhampansuriya2000@gmail.com>
2023-12-11 18:07:42 +0530

    add cursor pointer

--- src/components/Subnavbar.tsx
+++ src/components/Subnavbar.tsx
@@ -3,23 +3,23 @@ const Subnavbar = ({data} : any) => {
   return (
     <div className="rounded-sm border border-stroke bg-white py-4 px-5 shadow-default dark:border-strokedark dark:bg-boxdark">
       <div className=" flex items-center gap-[20px]">
-        <div className='{/*bg-[#91b5ff]*/} border-b-[3px] border-solid border-[#3d93ea] {/*rounded-[5px]*/} px-[10px] py-[15px]'>
+        <div className='{/*bg-[#91b5ff]*/} border-b-[3px] border-solid border-[#3d93ea] {/*rounded-[5px]*/} px-[10px] py-[15px] cursor-pointer'>
           <div className="text-title-sm font-bold text-black dark:text-white">
             Progressive Objective
           </div>
         </div>
         <div>
-          <div className="text-title-sm font-bold text-black dark:text-white">
+          <div className="text-title-sm font-bold text-black dark:text-white cursor-pointer">
             Advanced insights
           </div>
         </div>
         <div>
-          <div className="text-title-sm font-bold text-black dark:text-white">
+          <div className="text-title-sm font-bold text-black dark:text-white cursor-pointer">
             Trading Jounal
           </div>
         </div>
         <div>
-          <div className="text-title-sm font-bold text-black dark:text-white">
+          <div className="text-title-sm font-bold text-black dark:text-white cursor-pointer">
             Account Info
           </div>
         </div>
